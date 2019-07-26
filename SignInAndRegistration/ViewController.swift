4373a3368695d16fa3fdb1a2de82408d673eca23
dhruvil <dhruvil.patel29@gmail.com>
2019-07-26 12:14:32 -0400

    Initial Commit

---
+++ SignInAndRegistration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignInAndRegistration
+//
+//  Created by Canadore college on 2019-07-26.
+//  Copyright © 2019 dhruvil team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
